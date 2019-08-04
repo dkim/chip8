3aba9c0045f9a3951f5099a43044c022a56e3df6
Deokhwan Kim <dkim@users.noreply.github.com>
2019-08-04 17:12:36 -0400

    feat: accept a ROM filename as a command-line argument

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
 #![warn(rust_2018_idioms)]
 
-fn main() {}
+use std::{error::Error, path::PathBuf, process};
+
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(about)]
+struct Opt {
+    /// Sets a ROM file to run
+    #[structopt(name = "ROM-FILE", parse(from_os_str))]
+    rom_file: PathBuf,
+}
+
+fn main() {
+    if let Err(err) = run(Opt::from_args()) {
+        eprintln!("Error: {}", err);
+        process::exit(1);
+    }
+}
+
+fn run(_opt: Opt) -> Result<(), Box<dyn Error>> {
+    Ok(())
+}
